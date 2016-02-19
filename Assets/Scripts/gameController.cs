de84d95cc6100a2b81925a039077038a1b782106
James Doran <jysdoran@gmail.com>
2016-02-19 13:00:17 +0800

    Initial Commit
    Only a grid of buttons and some settings at the moment

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour
+{
+    public int currentTile;
+
+
+    void Start()
+    {
+        currentTile = 0;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+    public void selectTile (int tile)
+    {
+        currentTile = tile;
+        Debug.Log(currentTile);
+    }
+}
