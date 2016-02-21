2415f34bd29e7114649a24b49180588ca2641433
James Doran <jysdoran@gmail.com>
2016-02-21 14:43:53 +0800

    Add ability to addOccupants to tiles

---
+++ Assets/Scripts/foodBrain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class foodBrain : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -3,12 +3,35 @@ using System.Collections;
 
 public class gameController : MonoBehaviour
 {
-    public int currentTile;
+    Tile currentTile;
+    Tile[,] tileGrid;
 
+    class Tile
+    {
+        public GameObject occupant;
+        public int i;
+        public int j;
+
+        public Tile(int x, int y)
+        {
+            i = x;
+            j = y;
+        }
+    }
 
     void Start()
     {
-        currentTile = 0;
+        tileGrid = new Tile[3,5];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                tileGrid[i, j] = new Tile(i,j);
+            }
+        }
+
+        currentTile = tileGrid[1, 2];
+        addOccupant(currentTile, Resources.Load("Food"));
     }
 
     void Update()
@@ -16,11 +39,15 @@ public class gameController : MonoBehaviour
 
     }
 
-
-
-    public void selectTile (int tile)
+    void addOccupant(Tile t, Object o)
     {
-        currentTile = tile;
-        Debug.Log(currentTile);
+        t.occupant = Instantiate(o, new Vector3(t.i - 1,t.j - 2,0) * 1.5f, transform.rotation) as GameObject;
+    }
+
+    public void selectTile(int tile)
+    {
+        currentTile = tileGrid[tile % 10,Mathf.FloorToInt(tile/10)]; //Int -> array position
+
+        addOccupant(currentTile, Resources.Load("Food"));
     }
 }
